bbb4f41d9094adf888f63ae80ad89f85196504ff
Adam Rackis <adamrackis@gmail.com>
2019-11-28 16:47:33 -0600

    Remove render props api

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import Client, { defaultClientManager } from "./client";
 import compress from "./compress";
-import GraphQL from "./gqlComponent";
 import Cache from "./cache";
 import useQuery from "./useQuery";
 import useMutation from "./useMutation";
@@ -8,4 +7,4 @@ import { buildQuery, buildMutation } from "./util";
 
 const { setDefaultClient, getDefaultClient } = defaultClientManager;
 
-export { Client, compress, setDefaultClient, getDefaultClient, GraphQL, buildQuery, buildMutation, Cache, useQuery, useMutation };
+export { Client, compress, setDefaultClient, getDefaultClient, buildQuery, buildMutation, Cache, useQuery, useMutation };
