be41b73c7ab2b48d93f76d5a728d8d8d239815e2
Adam Rackis <adamrackis@gmail.com>
2020-08-08 14:22:33 -0500

    Re-mount tests added

--- test/hooks/oneQuery_props.test.js
+++ test/hooks/oneQuery_props.test.js
@@ -112,3 +112,48 @@ test("Cached data while loading handled", async () => {
   await pause();
   expect(getProps()).toMatchObject(dataPacket({ tasks: [{ id: 1 }] }));
 });
+
+test("Promise in flight picked up - resolved - handled", async () => {
+  const [getProps1, ComponentToUse1] = getComponent();
+  const [getProps2, ComponentToUse2] = getComponent();
+
+  let pData = (client1.nextResult = deferred());
+  let { rerender1 } = render(<ComponentToUse1 a={1} unused={0} />);
+
+  await pause();
+
+  let { rerender2 } = render(<ComponentToUse2 a={1} unused={0} />);
+
+  await resolveDeferred(pData, { data: { tasks: [{ id: 1 }] } });
+  expect(getProps1()).toMatchObject(dataPacket({ tasks: [{ id: 1 }] }));
+  expect(getProps2()).toMatchObject(dataPacket({ tasks: [{ id: 1 }] }));
+});
+
+test("Promise in flight picked up - rejected - and handled", async () => {
+  const [getProps1, ComponentToUse1] = getComponent();
+  const [getProps2, ComponentToUse2] = getComponent();
+
+  let pData = (client1.nextResult = deferred());
+  let { rerender1 } = render(<ComponentToUse1 a={1} unused={0} />);
+
+  await pause();
+
+  let { rerender2 } = render(<ComponentToUse2 a={1} unused={0} />);
+
+  await rejectDeferred(pData, { message: "Hello" });
+  expect(getProps1()).toMatchObject(errorPacket({ message: "Hello" }));
+  expect(getProps2()).toMatchObject(errorPacket({ message: "Hello" }));
+});
+
+test("Reload query - see new data", async () => {
+  let pFirst = (client1.nextResult = deferred());
+  let { rerender } = render(<ComponentToUse a={1} unused={0} />);
+
+  await resolveDeferred(pFirst, { data: { tasks: [{ id: 1 }] } });
+  expect(getProps()).toMatchObject(dataPacket({ tasks: [{ id: 1 }] }));
+
+  let pSecond = (client1.nextResult = deferred());
+  getProps().reload();
+  await resolveDeferred(pSecond, { data: { tasks: [{ id: 2 }] } });
+  expect(getProps()).toMatchObject(dataPacket({ tasks: [{ id: 2 }] }));
+});
