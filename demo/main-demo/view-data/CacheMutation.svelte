1bb5e8ba13a52ca1d695c484344b892f9a392c44
Adam Rackis <adamrackis@gmail.com>
2020-11-22 13:15:57 -0600

    Manual cache update demo

--- demo/main-demo/view-data/CacheMutation.svelte
+++ demo/main-demo/view-data/CacheMutation.svelte
@@ -6,6 +6,47 @@
 
   const graphQLClient = getDefaultClient();
 
+  // const syncCollection = (current, newResultsLookup) => {
+  //   return current.map(item => {
+  //     const updatedItem = newResultsLookup.get(item._id);
+  //     return updatedItem ? Object.assign({}, item, updatedItem) : item;
+  //   });
+  // };
+
+  // graphQLClient.subscribeMutation([
+  //   {
+  //     when: /updateBooks?/,
+  //     run: ({ refreshActiveQueries }, resp, variables) => {
+  //       const cache = graphQLClient.getCache(BOOKS_QUERY);
+  //       const newResults = resp.updateBook ? [resp.updateBook.Book] : resp.updateBooks.Books;
+  //       const newResultsLookup = new Map(newResults.map(item => [item._id, item]));
+
+  //       for (let [uri, { data }] of cache.entries) {
+  //         data["allBooks"]["Books"] = syncCollection(data["allBooks"]["Books"], newResultsLookup);
+  //       }
+
+  //       refreshActiveQueries(BOOKS_QUERY);
+  //     }
+  //   }
+  // ]);
+
+  // graphQLClient.subscribeMutation([
+  //   {
+  //     when: /updateSubjects?/,
+  //     run: ({ refreshActiveQueries }, resp, variables) => {
+  //       const cache = graphQLClient.getCache(ALL_SUBJECTS_QUERY);
+  //       const newResults = resp.updateSubject ? [resp.updateSubject.Subject] : resp.updateSubjects.Subjects;
+  //       const newResultsLookup = new Map(newResults.map(item => [item._id, item]));
+
+  //       for (let [uri, { data }] of cache.entries) {
+  //         data["allSubjects"]["Subjects"] = syncCollection(data["allSubjects"]["Subjects"], newResultsLookup);
+  //       }
+
+  //       refreshActiveQueries(ALL_SUBJECTS_QUERY);
+  //     }
+  //   }
+  // ]);
+
   const syncCollection = (current, newResultsLookup) => {
     return current.map(item => {
       const updatedItem = newResultsLookup.get(item._id);
@@ -13,39 +54,32 @@
     });
   };
 
-  graphQLClient.subscribeMutation([
-    {
-      when: /updateBooks?/,
-      run: ({ refreshActiveQueries }, resp, variables) => {
-        const cache = graphQLClient.getCache(BOOKS_QUERY);
-        const newResults = resp.updateBook ? [resp.updateBook.Book] : resp.updateBooks.Books;
-        const newResultsLookup = new Map(newResults.map(item => [item._id, item]));
+  export const syncQueryToCache = (query, type) => {
+    graphQLClient.subscribeMutation([
+      {
+        when: new RegExp(`update${type}s?`),
+        run: ({ refreshActiveQueries }, resp, variables) => {
+          const cache = graphQLClient.getCache(query);
+          const newResults = resp[`update${type}`]
+            ? [resp[`update${type}`][type]]
+            : resp[`update${type}s`][`${type}s`];
+          const newResultsLookup = new Map(newResults.map(item => [item._id, item]));
 
-        for (let [uri, { data }] of cache.entries) {
-          data["allBooks"]["Books"] = syncCollection(data["allBooks"]["Books"], newResultsLookup);
+          for (let [uri, { data }] of cache.entries) {
+            data[`all${type}s`][`${type}s`] = syncCollection(
+              data[`all${type}s`][`${type}s`],
+              newResultsLookup
+            );
+          }
+
+          refreshActiveQueries(query);
         }
-
-        refreshActiveQueries(BOOKS_QUERY);
       }
-    }
-  ]);
+    ]);
+  };
 
-  graphQLClient.subscribeMutation([
-    {
-      when: /updateSubjects?/,
-      run: ({ refreshActiveQueries }, resp, variables) => {
-        const cache = graphQLClient.getCache(ALL_SUBJECTS_QUERY);
-        const newResults = resp.updateSubject ? [resp.updateSubject.Subject] : resp.updateSubjects.Subjects;
-        const newResultsLookup = new Map(newResults.map(item => [item._id, item]));
-
-        for (let [uri, { data }] of cache.entries) {
-          data["allSubjects"]["Subjects"] = syncCollection(data["allSubjects"]["Subjects"], newResultsLookup);
-        }
-
-        refreshActiveQueries(ALL_SUBJECTS_QUERY);
-      }
-    }
-  ]);
+  syncQueryToCache(BOOKS_QUERY, "Book");
+  syncQueryToCache(ALL_SUBJECTS_QUERY, "Subject");
 
   let searchState = getContext("search_params");
 
