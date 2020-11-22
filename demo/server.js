b53f037f4e93052a70c97bbc86aa6fbe6ff0bfe2
Adam Rackis <adamrackis@gmail.com>
2020-11-22 16:36:16 -0600

    docs and demo updates

--- demo/server.js
+++ demo/server.js
@@ -7,7 +7,12 @@ const app = express();
 //app.get("/", (req, response) => response.sendFile(path.join(__dirname, "../docs/index.html")));
 app.get("/", (req, response) => response.sendFile(path.join(__dirname, "./dist/index.html")));
 app.get("/demo", (req, response) => response.sendFile(path.join(__dirname, "./dist/index.html")));
-//app.get("/docs", (req, response) => response.sendFile(path.join(__dirname, "../docs/index.html")));
+app.get("/docs", (req, response) => response.sendFile(path.join(__dirname, "../docs/index.html")));
+app.get("/docup.min.css", (req, response) => response.sendFile(path.join(__dirname, "../docs/docup.min.css")));
+app.get("/overrides.css", (req, response) => response.sendFile(path.join(__dirname, "../docs/overrides.css")));
+app.get("/docup.min.js", (req, response) => response.sendFile(path.join(__dirname, "../docs/docup.min.js")));
+app.get("/docs.md", (req, response) => response.sendFile(path.join(__dirname, "../docs/README.md")));
+app.get("/svelte-logo.png", (req, response) => response.sendFile(path.join(__dirname, "../docs/svelte-logo.png")));
 //app.get("/slides", (req, response) => response.sendFile(path.join(__dirname, "../graphql-texas-slides/dist/index.html")));
 //app.get("/deck.js", (req, response) => response.sendFile(path.join(__dirname, "../graphql-texas-slides/dist/deck.js")));
 // app.get("/docup.min.css", (req, response) => response.sendFile(path.join(__dirname, "../docs/docup.min.css")));
@@ -17,5 +22,6 @@ app.get("/demo", (req, response) => response.sendFile(path.join(__dirname, "./di
 // app.get("/docs.md", (req, response) => response.sendFile(path.join(__dirname, "../docs/README.md")));
 
 app.use("/dist/", express.static(__dirname + "/dist/"));
+
 
 app.listen(process.env.PORT || 8082);
